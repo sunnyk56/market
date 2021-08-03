6d96fafc7f2c5073ade2a68c8189cb1afccdc699
sunny960 <sunnypardhan80@gmail.com>
2021-08-03 13:09:43 +0530

    trying to run test in single container24

--- deploy/master-cosmos-orchestrator-node/scripts/setup-validator.sh
+++ deploy/master-cosmos-orchestrator-node/scripts/setup-validator.sh
@@ -1,100 +1,96 @@
 #!/bin/bash
-set -eux
-# your gaiad binary name
-BIN=gravity
+set -eu
 
-CHAIN_ID="gravity-test"
+echo "building environment"
 
-NODES=$1
+# Initial dir
+CURRENT_WORKING_DIR=$(pwd)
+# Name of the network to bootstrap
+CHAINID="testchain"
+# Name of the gravity artifact
+GRAVITY=gravity
+# The name of the gravity node
+GRAVITY_NODE_NAME="gravity"
+# The address to run gravity node
+GRAVITY_HOST="0.0.0.0"
+# Home folder for gravity config
+GRAVITY_HOME="$CURRENT_WORKING_DIR/$CHAINID/$GRAVITY_NODE_NAME"
+# Home flag for home folder
+GRAVITY_HOME_FLAG="--home $GRAVITY_HOME"
+# Config directories for gravity node
+GRAVITY_HOME_CONFIG="$GRAVITY_HOME/config"
+# Config file for gravity node
+GRAVITY_NODE_CONFIG="$GRAVITY_HOME_CONFIG/config.toml"
+# App config file for gravity node
+GRAVITY_APP_CONFIG="$GRAVITY_HOME_CONFIG/app.toml"
+# Keyring flag
+GRAVITY_KEYRING_FLAG="--keyring-backend test"
+# Chain ID flag
+GRAVITY_CHAINID_FLAG="--chain-id $CHAINID"
+# The name of the gravity validator
+GRAVITY_VALIDATOR_NAME=val
+# The name of the gravity orchestrator/validator
+GRAVITY_ORCHESTRATOR_NAME=orch
+# Gravity chain demons
+STAKE_DENOM="stake"
+#NORMAL_DENOM="samoleans"
+NORMAL_DENOM="footoken"
 
-ALLOCATION="10000000000stake,10000000000footoken"
-
+# This key is the private key for the public key defined in ETHGenesis.json
+# where the full node / miner sends its rewards. Therefore it's always going
+# to have a lot of ETH to pay for things like contract deployments
+#ETH_MINER_PRIVATE_KEY="0xb1bab011e03a9862664706fc3bbaa1b16651528e5f0e7fbfcbfdd8be302a13e7"
+#ETH_MINER_PUBLIC_KEY="0xBf660843528035a5A4921534E156a27e64B231fE"
 ETH_MINER_PRIVATE_KEY="0xb1bab011e03a9862664706fc3bbaa1b16651528e5f0e7fbfcbfdd8be302a13e7"
 ETH_MINER_PUBLIC_KEY="0xBf660843528035a5A4921534E156a27e64B231fE"
+# The host of ethereum node
+ETH_HOST="0.0.0.0"
 
-# first we start a genesis.json with validator 1
-# validator 1 will also collect the gentx's once gnerated
-STARTING_VALIDATOR=1
-STARTING_VALIDATOR_HOME="--home /root/testchain/gravity"
+# ------------------ Init gravity ------------------
 
-# Config file for gravity node
-GRAVITY_NODE_CONFIG="/root/testchain/gravity/config/config.toml"
-# App config file for gravity node
-GRAVITY_APP_CONFIG="/root/testchain/gravity/config/app.toml"
-# todo add git hash to chain name
-$BIN init $STARTING_VALIDATOR_HOME --chain-id=$CHAIN_ID validator1
+echo "Creating $GRAVITY_NODE_NAME validator with chain-id=$CHAINID..."
+echo "Initializing genesis files"
+# Build genesis file incl account for passed address
+GRAVITY_GENESIS_COINS="100000000000$STAKE_DENOM,100000000000$NORMAL_DENOM"
 
+# Initialize the home directory and add some keys
+echo "Init test chain"
+$GRAVITY $GRAVITY_HOME_FLAG $GRAVITY_CHAINID_FLAG init $GRAVITY_NODE_NAME
 
-## Modify generated genesis.json to our liking by editing fields using jq
-## we could keep a hardcoded genesis file around but that would prevent us from
-## testing the generated one with the default values provided by the module.
 
 # add in denom metadata for both native tokens
-jq '.app_state.bank.denom_metadata += [{"base": "footoken", display: "mfootoken", "description": "A non-staking test token", "denom_units": [{"denom": "footoken", "exponent": 0}, {"denom": "mfootoken", "exponent": 6}]}, {"base": "stake", display: "mstake", "description": "A staking test token", "denom_units": [{"denom": "stake", "exponent": 0}, {"denom": "mstake", "exponent": 6}]}]' /root/testchain/gravity/config/genesis.json > /metadata-genesis.json
+jq '.app_state.bank.denom_metadata += [{"base": "footoken", display: "mfootoken", "description": "A non-staking test token", "denom_units": [{"denom": "footoken", "exponent": 0}, {"denom": "mfootoken", "exponent": 6}]}, {"base": "stake", display: "mstake", "description": "A staking test token", "denom_units": [{"denom": "stake", "exponent": 0}, {"denom": "mstake", "exponent": 6}]}]' $GRAVITY_HOME_CONFIG/genesis.json > /metadata-genesis.json
 
 # a 60 second voting period to allow us to pass governance proposals in the tests
 jq '.app_state.gov.voting_params.voting_period = "60s"' /metadata-genesis.json > /edited-genesis.json
-
 mv /edited-genesis.json /genesis.json
+mv /genesis.json $GRAVITY_HOME_CONFIG/genesis.json
 
+echo "Add validator key"
+$GRAVITY $GRAVITY_HOME_FLAG keys add $GRAVITY_VALIDATOR_NAME $GRAVITY_KEYRING_FLAG --output json | jq . >> $GRAVITY_HOME/validator_key.json
+echo "Generating orchestrator keys"
+$GRAVITY $GRAVITY_HOME_FLAG keys add --output=json $GRAVITY_ORCHESTRATOR_NAME | jq . >> $GRAVITY_HOME/orchestrator_key.json
+echo "Adding validator addresses to genesis files"
+$GRAVITY $GRAVITY_HOME_FLAG add-genesis-account "$($GRAVITY $GRAVITY_HOME_FLAG keys show $GRAVITY_VALIDATOR_NAME -a $GRAVITY_KEYRING_FLAG)" $GRAVITY_GENESIS_COINS
+echo "Adding orchestrator addresses to genesis files"
+$GRAVITY $GRAVITY_HOME_FLAG add-genesis-account "$($GRAVITY $GRAVITY_HOME_FLAG keys show $GRAVITY_ORCHESTRATOR_NAME -a $GRAVITY_KEYRING_FLAG)" $GRAVITY_GENESIS_COINS
 
-# Sets up an arbitrary number of validators on a single machine by manipulating
-# the --home parameter on gaiad
-for i in $(seq 1 $NODES);
-do
-GAIA_HOME="--home /root/testchain/gravity"
-GENTX_HOME="--home-client /root/testchain/gravity"
-ARGS="$GAIA_HOME --keyring-backend test"
+#echo "Adding orchestrator keys to genesis"
+#GRAVITY_ORCHESTRATOR_KEY="$(jq .address $GRAVITY_HOME/orchestrator_key.json)"
 
-# Generate a validator key, orchestrator key, and eth key for each validator
-$BIN keys add $ARGS validator$i 2>> /validator-phrases
-$BIN keys add $ARGS orchestrator$i 2>> /orchestrator-phrases
-$BIN eth_keys add >> /validator-eth-keys
+#jq ".app_state.auth.accounts += [{\"@type\": \"/cosmos.auth.v1beta1.BaseAccount\",\"address\": $GRAVITY_ORCHESTRATOR_KEY,\"pub_key\": null,\"account_number\": \"0\",\"sequence\": \"0\"}]" $GRAVITY_HOME_CONFIG/genesis.json | sponge $GRAVITY_HOME_CONFIG/genesis.json
+#jq ".app_state.bank.balances += [{\"address\": $GRAVITY_ORCHESTRATOR_KEY,\"coins\": [{\"denom\": \"$NORMAL_DENOM\",\"amount\": \"100000000000\"},{\"denom\": \"$STAKE_DENOM\",\"amount\": \"100000000000\"}]}]" $GRAVITY_HOME_CONFIG/genesis.json | sponge $GRAVITY_HOME_CONFIG/genesis.json
 
-VALIDATOR_KEY=$($BIN keys show validator$i -a $ARGS)
-ORCHESTRATOR_KEY=$($BIN keys show orchestrator$i -a $ARGS)
-# move the genesis in
-mkdir -p /root/testchain/gravity/config/
-mv /genesis.json /root/testchain/gravity/config/genesis.json
-$BIN add-genesis-account $ARGS $VALIDATOR_KEY $ALLOCATION
-$BIN add-genesis-account $ARGS $ORCHESTRATOR_KEY $ALLOCATION
-# move the genesis back out
-mv /root/testchain/gravity/config/genesis.json /genesis.json
-done
+echo "Generating ethereum keys"
+$GRAVITY $GRAVITY_HOME_FLAG eth_keys add --output=json | jq . >> $GRAVITY_HOME/eth_key.json
 
+echo "Creating gentxs"
+$GRAVITY $GRAVITY_HOME_FLAG gentx --ip $GRAVITY_HOST $GRAVITY_VALIDATOR_NAME 100000000000$STAKE_DENOM "$(jq -r .address $GRAVITY_HOME/eth_key.json)" "$(jq -r .address $GRAVITY_HOME/orchestrator_key.json)" $GRAVITY_KEYRING_FLAG $GRAVITY_CHAINID_FLAG
 
-for i in $(seq 1 $NODES);
-do
-cp /genesis.json /root/testchain/gravity/config/genesis.json
-GAIA_HOME="--home /root/testchain/gravity"
-ARGS="$GAIA_HOME --keyring-backend test"
-ORCHESTRATOR_KEY=$($BIN keys show orchestrator$i -a $ARGS)
-ETHEREUM_KEY=$(grep address /validator-eth-keys | sed -n "$i"p | sed 's/.*://')
-# the /8 containing 7.7.7.7 is assigned to the DOD and never routable on the public internet
-# we're using it in private to prevent gaia from blacklisting it as unroutable
-# and allow local pex
-$BIN gentx $ARGS $GAIA_HOME --moniker validator$i --chain-id=$CHAIN_ID --ip 0.0.0.0 validator$i 500000000stake $ETHEREUM_KEY $ORCHESTRATOR_KEY
-# obviously we don't need to copy validator1's gentx to itself
-if [ $i -gt 1 ]; then
-cp /validator$i/config/gentx/* /root/testchain/gravity/config/gentx/
-fi
-done
+echo "Collecting gentxs in $GRAVITY_NODE_NAME"
+$GRAVITY $GRAVITY_HOME_FLAG collect-gentxs
 
-
-$BIN collect-gentxs $STARTING_VALIDATOR_HOME
-GENTXS=$(ls /root/testchain/gravity/config/gentx | wc -l)
-cp /root/testchain/gravity/config/genesis.json /genesis.json
-echo "Collected $GENTXS gentx"
-
-# put the now final genesis.json into the correct folders
-for i in $(seq 1 $NODES);
-do
-cp /genesis.json /root/testchain/gravity/config/genesis.json
-done
-
-
-echo "Adding initial ethereum value for miner"
-jq ".alloc |= . + {\"$ETH_MINER_PUBLIC_KEY\" : {\"balance\": \"0x1337000000000000000000\"}}" assets/ETHGenesis.json | sponge assets/ETHGenesis.json
+echo "Exposing ports and APIs of the $GRAVITY_NODE_NAME"
 # Switch sed command in the case of linux
 fsed() {
   if [ `uname` = 'Linux' ]; then
@@ -103,9 +99,19 @@ fsed() {
     sed -i '' "$@"
   fi
 }
-fsed "s#\"tcp://127.0.0.1:26656\"#\"tcp://0.0.0.0:26656\"#g" $GRAVITY_NODE_CONFIG
-fsed "s#\"tcp://127.0.0.1:26657\"#\"tcp://0.0.0.0:26657\"#g" $GRAVITY_NODE_CONFIG
+
+# Change ports
+fsed "s#\"tcp://127.0.0.1:26656\"#\"tcp://$GRAVITY_HOST:26656\"#g" $GRAVITY_NODE_CONFIG
+fsed "s#\"tcp://127.0.0.1:26657\"#\"tcp://$GRAVITY_HOST:26657\"#g" $GRAVITY_NODE_CONFIG
 fsed 's#addr_book_strict = true#addr_book_strict = false#g' $GRAVITY_NODE_CONFIG
-fsed 's#external_address = ""#external_address = "tcp://0.0.0.0:26656"#g' $GRAVITY_NODE_CONFIG
+fsed 's#external_address = ""#external_address = "tcp://'$GRAVITY_HOST:26656'"#g' $GRAVITY_NODE_CONFIG
 fsed 's#enable = false#enable = true#g' $GRAVITY_APP_CONFIG
 fsed 's#swagger = false#swagger = true#g' $GRAVITY_APP_CONFIG
+
+echo "Adding initial ethereum value for miner"
+jq ".alloc |= . + {\"$ETH_MINER_PUBLIC_KEY\" : {\"balance\": \"0x1337000000000000000000\"}}" assets/ETHGenesis.json | sponge assets/ETHGenesis.json
+
+#echo "Adding initial ethereum value for gravity validator"
+#jq ".alloc |= . + {$(jq .address $GRAVITY_HOME/eth_key.json) : {\"balance\": \"0x1337000000000000000000\"}}" assets/ETHGenesis.json | sponge assets/ETHGenesis.json
+
+
