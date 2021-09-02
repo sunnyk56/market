3879032dbcf19d0438ae17b901b71bb88bb47a46
sunny960 <sunnypardhan80@gmail.com>
2021-09-02 16:39:27 +0530

    onomy chain setup

--- deploy/onomy-chain/bin.sh
+++ deploy/onomy-chain/bin.sh
@@ -19,37 +19,39 @@ echo "-----------Installing_dependencies---------------"
  dnf -y install wget
  dnf -y install screen
 
+HOME_DIRECTORY=$HOME
 
 echo "--------------installing_rust---------------------------"
 
 curl https://sh.rustup.rs -sSf | bash -s -- -y
-export PATH=$HOME/.cargo/bin:$PATH
+export PATH=$HOME_DIRECTORY/.cargo/bin:$PATH
 cargo version
 
 echo "----------------cloning_repository-------------------"
 
-GRAVITY_DIR=$HOME/gravity
-ONOMY_DIR=$HOME/go/onomy
+GRAVITY_DIR=$HOME_DIRECTORY/gravity
+ONOMY_DIR=$HOME_DIRECTORY/go/onomy
 git clone https://github.com/onomyprotocol/cosmos-gravity-bridge.git $GRAVITY_DIR
-git clone -b dzmitryhil/gravity-init https://github.com/onomyprotocol/onomy.git $ONOMY_DIR
+git clone https://github.com/sunnyk56/onomy-1.git $ONOMY_DIR
+#git clone -b dzmitryhil/gravity-init https://github.com/onomyprotocol/onomy.git $ONOMY_DIR
 
 echo "--------------install_golang---------------------------"
-curl https://dl.google.com/go/go1.16.4.linux-amd64.tar.gz --output $HOME/go.tar.gz
-tar -C $HOME -xzf $HOME/go.tar.gz
-export PATH=$PATH:$HOME/go/bin
+curl https://dl.google.com/go/go1.16.4.linux-amd64.tar.gz --output $HOME_DIRECTORY/go.tar.gz
+tar -C $HOME_DIRECTORY -xzf $HOME_DIRECTORY/go.tar.gz
+export PATH=$PATH:$HOME_DIRECTORY/go/bin
 
 echo "----------------------building_gravity_artifact---------------"
 #cd $GRAVITY_DIR/module
 #make install
 cd $ONOMY_DIR
 make build
-cp onomyd $HOME/go/bin/onomyd
+cp onomyd $HOME_DIRECTORY/go/bin/onomyd
 
 echo "----------------building_orchestrator_artifact-------------"
 cd $GRAVITY_DIR/orchestrator
 rustup target add x86_64-unknown-linux-musl
 cargo build --target=x86_64-unknown-linux-musl --release  --all
-cp $GRAVITY_DIR/orchestrator/target/x86_64-unknown-linux-musl/release/gbt $HOME/go/bin/gbt
+cp $GRAVITY_DIR/orchestrator/target/x86_64-unknown-linux-musl/release/gbt $HOME_DIRECTORY/go/bin/gbt
 
 
 echo "---------------Installing_solidity-------------------"
@@ -61,19 +63,19 @@ npm run typechain
 
 
 echo "-------------------making_geth-----------------------"
-cd $HOME
+cd $HOME_DIRECTORY
 git clone https://github.com/ethereum/go-ethereum
 cd go-ethereum/
 make geth
-cp build/bin/geth $HOME/go/bin/geth
+cp build/bin/geth $HOME_DIRECTORY/go/bin/geth
 
 echo "------------------ install fauset ------------------"
 curl https://get.starport.network/faucet! | bash
 
 # dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 # sudo subscription-manager repos --enable codeready-builder-for-rhel-8-x86_64-rpms
-export PATH=$PATH:$HOME/go/bin
-cd $HOME
+export PATH=$PATH:$HOME_DIRECTORY/go/bin
+cd $HOME_DIRECTORY
 
 
 
